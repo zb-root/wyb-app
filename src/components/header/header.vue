f369e463f0c8c27dfe431762d726ebb24f1ecf05
Qukoko <15016630905@163.com>
2018-05-07 18:55:25 +0800

    修改头部返回箭头

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -2,7 +2,8 @@
   <div>
     <mt-header v-bind:title='title' fixed style="height:3rem; font-size:1.2em;background-color:#1E1E1E">/*background-color:#1A4B9C*/
       <router-link v-bind:to="leftNav" slot="left" v-if="isLeftNav">
-        <mt-button icon="back" style="font-size:0.8em">返回</mt-button>
+        <!--<mt-button icon="back" style="font-size:0.8em">返回</mt-button>-->
+        <mt-button style="font-size:0.8em;background-size:100% 100%;width:1.5rem;height:1rem;" v-bind:style="{backgroundImage:'url('+backImg+')'}"></mt-button>
       </router-link>
       <router-link v-bind:to="rightNav" slot="right" v-if="isRightTo">
         <mt-button style="font-size:0.8em">{{rightText}}</mt-button>
@@ -20,7 +21,8 @@
     data () {
       return {
       	isLeftNav:this.leftNav ? true : false,
-        isRightTo: this.rightNav ? true :false
+        isRightTo: this.rightNav ? true :false,
+        backImg:require('../../assets/png/back.png')
       }
     },
     mounted: function () {
